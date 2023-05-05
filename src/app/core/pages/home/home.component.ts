eaf6f0b389f9e9e5c56f49825c13b99e0c471902
jair <jair.lopes@fatec.sp.gov.br>
2023-05-04 23:44:23 -0300

    Created a new resource to get all names of project

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repos } from 'src/app/models/repos.model';
+import { RepoService } from '../../../services/repo.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  repos: string[] = [];
+
+  constructor(private _repoService: RepoService) { }
 
   ngOnInit(): void {
+    this.getRepos();
   }
 
+  public getRepos = (): void => {
+    this._repoService.getCityRegistered()
+    .pipe(
+      map((repos: Repos[]) => repos.map(rr => rr.name))
+    )
+    .subscribe({
+      next: (response) => {
+        this.repos = response;
+      },
+      error: (err: any) => {
+      }
+    });
+  }
 }

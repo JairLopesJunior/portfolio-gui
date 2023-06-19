bd41740f514f5b1f891dfa0db3ff95fef6d7e82c
jair <JairLopesJunior>
2023-06-19 20:37:46 -0300

    Adiiconado os cards da home para o portfolio

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Repos } from 'src/app/models/repos.model';
-import { RepoService } from '../../../services/repo.service';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -10,25 +7,8 @@ import { map } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
 
-  repos: string[] = [];
-
-  constructor(private _repoService: RepoService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getRepos();
-  }
-
-  public getRepos = (): void => {
-    this._repoService.getCityRegistered()
-    .pipe(
-      map((repos: Repos[]) => repos.map(rr => rr.name))
-    )
-    .subscribe({
-      next: (response) => {
-        this.repos = response;
-      },
-      error: (err: any) => {
-      }
-    });
   }
 }
--- src/app/core/pages/portfolio/portfolio.component.ts
+++ src/app/core/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RepoService } from 'src/app/services/repo.service';
+import { map } from 'rxjs/operators';
+import { Repos } from 'src/app/models/repos.model';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  repos: string[] = [];
+
+  constructor(private _repoService: RepoService) { }
 
   ngOnInit(): void {
+    this.getRepos();
   }
 
+  public getRepos = (): void => {
+    this._repoService.getCityRegistered()
+    .pipe(
+      map((repos: Repos[]) => repos.map(rr => rr.name))
+    )
+    .subscribe({
+      next: (response) => {
+        this.repos = response;
+      },
+      error: (err: any) => {
+      }
+    });
+  }
 }

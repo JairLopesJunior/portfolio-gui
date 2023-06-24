4d3e1cc6ed22f3622be1eea0724908eb55670f14
unknown <jair.lopes@fatec.sp.gov.br>
2023-06-24 15:51:53 -0300

    Finalizado a tela de UQEM SOU

--- src/app/core/pages/quem-sou/quem-sou.component.ts
+++ src/app/core/pages/quem-sou/quem-sou.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProgressBar } from 'src/app/models/progress-bar.model';
 
 @Component({
   selector: 'app-quem-sou',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuemSouComponent implements OnInit {
 
+  progressBarItems: ProgressBar[] = [
+    {
+      titulo: 'JAVA',
+      valuenow: 70
+    },
+    {
+      titulo: 'JAVASCRIPT',
+      valuenow: 66
+    },
+    {
+      titulo: 'TYPESCRIPT',
+      valuenow: 70
+    },
+    {
+      titulo: 'SPRING BOOT',
+      valuenow: 70
+    },
+    {
+      titulo: 'MYSQL',
+      valuenow: 50
+    },
+    {
+      titulo: 'POSTGRESQL',
+      valuenow: 50
+    },
+    {
+      titulo: 'ANGULARJS',
+      valuenow: 50
+    },
+    {
+      titulo: 'ANGULAR2+',
+      valuenow: 70
+    },
+    {
+      titulo: 'RXJS',
+      valuenow: 45
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/models/progress-bar.model.ts
+++ src/app/models/progress-bar.model.ts
@@ -1,4 +1,4 @@
 export class ProgressBar {
-    valuenow?: number;
     titulo?: string;
+    valuenow?: number;
 }

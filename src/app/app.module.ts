46e3bd531b7f63dc002a52f5618d7b358fd13eb6
unknown <jair.lopes@fatec.sp.gov.br>
2023-06-30 00:13:33 -0300

    Adicionado icones e descrição na tela de CONTATO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './core/pages/pages.module';
-import { InputTextComponent } from './share/components/input-text/input-text.component';
 
 const customNotifierOptions: NotifierOptions = {
   position: {
@@ -52,8 +51,7 @@ const customNotifierOptions: NotifierOptions = {
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InputTextComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

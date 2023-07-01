f0ce9219b5b2b3b5c9c3983cd86e885de958ad2c
unknown <jair.lopes@fatec.sp.gov.br>
2023-07-01 16:31:47 -0300

    Adicionado logica no novo component campo-control-erro

--- src/app/shared/components/campo-control-erro/campo-control-erro.component.ts
+++ src/app/shared/components/campo-control-erro/campo-control-erro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-campo-control-erro',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampoControlErroComponent implements OnInit {
 
+  @Input() msgErro: string;
+  @Input() mostrarErro: boolean | undefined;
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
